6a015b09bd5472e92135e477771407e9adc95d79
linzhiqiang <linzhiqiang@shandagames.com>
2021-03-16 14:37:19 +0800

    test

---
+++ examples/Aix.RedisStreamMessageBus.Sample/MessagePackSerializerImpl.cs
@@ -0,0 +1,53 @@
+﻿using Aix.RedisStreamMessageBus.Serializer;
+using MessagePack;
+using MessagePack.Resolvers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aix.RedisStreamMessageBus.Sample
+{
+    public class MessagePackSerializerImpl : ISerializer
+    {
+        private readonly IFormatterResolver _formatterResolver;
+        private readonly bool _useCompression;
+
+        private MessagePackSerializerOptions _compressionOptions;
+        private MessagePackSerializerOptions _unCompressionOptions;
+
+        public MessagePackSerializerImpl(IFormatterResolver resolver = null, bool useCompression = false)
+        {
+            _useCompression = useCompression;
+            _formatterResolver = resolver ?? ContractlessStandardResolver.Instance;
+
+            _compressionOptions = MessagePackSerializerOptions.Standard.WithResolver(_formatterResolver).WithCompression(MessagePackCompression.Lz4BlockArray);
+            _unCompressionOptions = MessagePackSerializerOptions.Standard.WithResolver(_formatterResolver);
+        }
+
+        public T Deserialize<T>(byte[] bytes)
+        {
+            if (_useCompression)
+            {
+                return MessagePack.MessagePackSerializer.Deserialize<T>(bytes, _compressionOptions);
+            }
+            else
+            {
+                return MessagePack.MessagePackSerializer.Deserialize<T>(bytes, _unCompressionOptions);
+            }
+        }
+
+        public byte[] Serialize<T>(T data)
+        {
+            if (_useCompression)
+            {
+                return MessagePack.MessagePackSerializer.Serialize(data, _compressionOptions);
+            }
+            else
+            {
+                return MessagePack.MessagePackSerializer.Serialize(data, _unCompressionOptions);
+            }
+        }
+    }
+}
--- examples/Aix.RedisStreamMessageBus.Sample/Startup.cs
+++ examples/Aix.RedisStreamMessageBus.Sample/Startup.cs
@@ -15,30 +15,23 @@ namespace Aix.RedisStreamMessageBus.Sample
     {
         internal static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
         {
-            Console.WriteLine("*************************");
-            Console.WriteLine(context.HostingEnvironment.EnvironmentName);
-            Console.WriteLine("*************************");
-
+            //控制台命令行启动参数解析
             var options = CmdOptions.Options;
             services.AddSingleton(options);
 
             #region messagebus相关
+
             var redisMessageBusOptions = context.Configuration.GetSection("redis-messagebus").Get<RedisMessageBusOptions>();
-            //异常处理，该异常是否需要进行任务重试  最好根据错误码判断，只有系统异常才进行重试
+            //这里可以覆盖默认的序列化方式，内部默认是system.txt.json实现，这里替换为messagepack
+            redisMessageBusOptions.Serializer = new MessagePackSerializerImpl();
 
-            //ConfigurationOptions options1 = new ConfigurationOptions();
-            //options1.EndPoints.Add("127.0.0.1:6379");
-            //options1.EndPoints.Add("127.0.0.1:6380");
-            //ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(options1);
-            //redisMessageBusOptions.ConnectionMultiplexer = connection;
-
-            services.AddRedisMessageBus(redisMessageBusOptions); //list实现
-                                                                 //services.AddRedisMessageBusPubSub(redisMessageBusOptions);//发布订阅实现
+            services.AddRedisMessageBus(redisMessageBusOptions); //stream实现
+            //services.AddRedisMessageBusPubSub(redisMessageBusOptions);//发布订阅实现
 
             #endregion
 
 
-
+            //这里为了测试方便，做了区分
             if ((options.Mode & (int)ClientMode.Consumer) > 0)
             {
                 services.AddHostedService<MessageBusConsumeService>();
@@ -49,11 +42,6 @@ namespace Aix.RedisStreamMessageBus.Sample
             }
         }
 
-        #region private 
-
-
-
-
-        #endregion
+       
     }
 }
